74c19c427587cccc3481172df50e7832ba1ad5d3
BALAND Jérome <baland.jerome@gmail.com>
2025-02-09 19:36:50 +0100

    Navbar avec JS

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // console.log("hello");
-
 const buttons = document.querySelectorAll(".btn");
 const slides = document.querySelectorAll(".slide");
+const Navbar = document.querySelector("nav");
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > 300 && innerWidth > 650) {
+    Navbar.style.position = "fixed";
+    Navbar.style.top = "0";
+  } else if (innerWidth <= 650 && window.scrollY > 521) {
+    Navbar.style.position = "fixed";
+    Navbar.style.top = "-221px";
+    nav_btn.style.opacity = "1";
+  } else {
+    Navbar.style.position = "absolute";
+    Navbar.style.top = "300px";
+    nav_btn.style.opacity = "0";
+  }
+});
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
